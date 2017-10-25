a182d6c7ffa82a9319ea77bcb13ea669936345c8
Adam Ossowski <adam@porajdesign.com>
2017-10-25 13:15:24 -0700

    added parse args function so all  can be pulled from cli flags

--- check_lag.py
+++ check_lag.py
@@ -4,9 +4,33 @@
 import os
 import sys
 import json
+import argparse
 from subprocess import Popen,PIPE,STDOUT
 from collections import namedtuple
 
+def parse_args():
+    """ Parse CLI arguments -  """
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(    "--community",
+                            dest='community',
+                            default='public',
+                            help="SNMP community set as public by default")
+
+    parser.add_argument(    "--unicast-ip",
+                            dest='uni_ip',
+                            help="Unicast IP address, can be specified by $HOSTADDRESS$ Nagios macro")
+
+    parser.add_argument(    "--agg-port",
+                            dest='aggregate_port',                    
+                            help="Specifies aggregate ethernet port." )
+
+    parser.add_argument(    "--port-speed",
+                            default='port_speed',
+                            help="Specifies port speed in mb/sec." )                          
+
+    args = parser.parse_args()
+    return args
 
 def shell_exec(command):
     """ subprocess wrapper function, returns named 
@@ -20,6 +44,10 @@ def shell_exec(command):
     resp = namedtuple("resp", ["stdout","stderr","returncode"])
     return resp(stdout,stderr,returncode)
 
+
+if __name__ == "__main__":
+    args = parse_args()
+    print args
 '''
 #!/bin/bash
 # r3lik 11/11/16
